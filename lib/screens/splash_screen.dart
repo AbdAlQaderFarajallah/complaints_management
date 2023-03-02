b0e2caafc40fb0aefa5a8634a3c4c782532ab622
AbdAlQaderFouadFarajallah <aboodfouad2000@gmail.com>
2023-03-02 19:34:09 +0200

    * splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_and_signup.dart';
+
+class SplashPage extends StatefulWidget {
+  static const String id = "SplashPage";
+
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacementNamed(context, LoginAndSignupPage.id),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: controller.value * 150,
+                    width: controller.value * 150,
+                    child: Image.asset('images/comments.png'),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  "Complaints Management System",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: controller.value * 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
