df344ec81f84f8ace021a3b0df0fd68f347a65e3
AbdAlQaderFouadFarajallah <aboodfouad2000@gmail.com>
2023-03-04 19:51:32 +0200

    * user update api

--- lib/api/api_settings.dart
+++ lib/api/api_settings.dart
@@ -3,5 +3,6 @@ class ApiSettings {
   static const login = '$_baseUrl/login';
   static const register = '$_baseUrl/register';
   static const logout = '$_baseUrl/logout';
+  static const userUpdate = '$_baseUrl/user/update';
 
 }
