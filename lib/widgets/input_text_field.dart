493da387fd46caf96222488b698725f7508dff60
AbdAlQaderFouadFarajallah <aboodfouad2000@gmail.com>
2023-03-08 12:22:47 +0200

    * edit input_text_field widget

--- lib/widgets/input_text_field.dart
+++ lib/widgets/input_text_field.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 class InputTextField extends StatelessWidget {
   InputTextField(
-      {super.key,
-      required this.icon,
+      {super.key, this.icon,
       required this.hintText,
       required this.textController});
 
   late TextEditingController textController;
-  IconData icon;
+  IconData? icon;
   String hintText;
 
   @override
@@ -16,7 +15,7 @@ class InputTextField extends StatelessWidget {
     return TextField(
       controller: textController,
       decoration: InputDecoration(
-        prefixIcon: Icon(icon),
+        prefixIcon:icon==null?null: Icon(icon),
         prefixIconColor: const Color(0xFF6589FF),
         border: const UnderlineInputBorder(),
         hintText: hintText,
