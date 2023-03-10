0f160c3ff86ce300e054c6e9f12c84cab781634f
AbdAlQaderFouadFarajallah <aboodfouad2000@gmail.com>
2023-03-10 10:59:19 +0200

    * edit search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:complaints_management/api/controllers/search/search_api_controller.dart';
 import 'package:complaints_management/models/search.dart';
+import 'package:complaints_management/screens/filters_screen.dart';
 import 'package:complaints_management/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -56,42 +57,38 @@ class _SearchPageState extends State<SearchPage> {
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
               Row(
                 children: [
                   Container(
                     width: 310,
                     decoration: const BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextField(
-                        onChanged: (value) async {
-                          if (value.isNotEmpty) {
-                            _search =
-                                await SearchApiController().search(text: value);
-                            setState(() {});
-                          } else {
-                            _search = [];
-                            setState(() {});
-                          }
-                        },
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.grey,
-                          ),
-                          hintText: "search",
-                          border: InputBorder.none,
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(25))),
+                    child: TextField(
+                      onChanged: (value) async {
+                        if (value.isNotEmpty) {
+                          _search =
+                              await SearchApiController().search(text: value);
+                          setState(() {});
+                        } else {
+                          _search = [];
+                          setState(() {});
+                        }
+                      },
+                      decoration: const InputDecoration(
+                        hintText: "Search",
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
                         ),
                       ),
                     ),
                   ),
                   const SizedBox(width: 10),
                   InkWell(
-                    onTap: () {},
+                    onTap: () {
+                      Navigator.pushNamed(context, FiltersScreen.id);
+                    },
                     child: const Icon(
                       size: 30,
                       Icons.filter_alt_outlined,
@@ -105,7 +102,132 @@ class _SearchPageState extends State<SearchPage> {
                 child: ListView.builder(
                     itemCount: _search.length,
                     itemBuilder: (context, index) {
-                      return Text(_search[index].subject!);
+                      // Text(_search[index].subject!);
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 16, right: 16),
+                          padding: const EdgeInsets.all(8),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(25)),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        height: 12,
+                                        width: 12,
+                                        decoration: const BoxDecoration(
+                                          color: Colors.blue,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(50),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 8,
+                                      ),
+                                      Text(
+                                        _search[index].sender!.name!,
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          _search[index].archiveNumber!,
+                                          style: const TextStyle(
+                                              color: Colors.grey),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 24),
+                                child: Text(
+                                  _search[index].subject!,
+                                  style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 15),
+                                ),
+                              ),
+                              const SizedBox(height: 2),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 24),
+                                child: Text(
+                                  "And here excerpt of the mail, can add to this location. And we can do more to this like add multi images",
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 24),
+                                child: Text(
+                                  _search[index].tags![0].name!,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 24),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 42,
+                                      width: 42,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.grey,
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(8),
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Image.asset(
+                                          "images/person.png",
+                                          width: 36,
+                                          height: 36,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
                     }),
               ),
             ],
